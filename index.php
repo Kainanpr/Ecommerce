c031118fb04391964f57756b62e3ad5eebd2f214
Kainanpr <Kainanpr95@gmail.com>
2018-03-06 15:37:45 -0300

    Início do projeto

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	$sql = new DB\Sql();
+
+	echo "OK";
+
+});
+
+$app->run();
+
+ ?>
